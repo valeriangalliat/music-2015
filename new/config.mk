a9ae7af1a5f55e0c26f217d7e0cff79f7d469791
Valérian Galliat <val@codejam.info>
2014-08-27 19:48:19 +0200

    Use only ID in name

--- new/config.mk
+++ new/config.mk
@@ -1,6 +1,7 @@
 YOUTUBE_DL = youtube-dl
 
 YOUTUBE_DL_FLAGS += --extract-audio
+YOUTUBE_DL_FLAGS += --id
 YOUTUBE_DL_FLAGS += --ignore-errors
 YOUTUBE_DL_FLAGS += --no-progress
 YOUTUBE_DL_FLAGS += --write-info-json
