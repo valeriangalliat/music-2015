261c7351e3b93c18c7b6fbb9070157363f3f5b2d
Valérian Galliat <val@codejam.info>
2014-09-28 11:46:25 +0200

    Download archive

--- new/config.mk
+++ new/config.mk
@@ -5,3 +5,4 @@ YOUTUBE_DL_FLAGS += --id
 YOUTUBE_DL_FLAGS += --ignore-errors
 YOUTUBE_DL_FLAGS += --no-progress
 YOUTUBE_DL_FLAGS += --write-info-json
+YOUTUBE_DL_FLAGS += --download-archive archive.log
